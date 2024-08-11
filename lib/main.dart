f893bf6c9137da1f2d4b3bb10f46aa4a735e155c
MyKnow <myknow000@gmail.com>
2024-08-12 03:12:35 +0900

    feat: apply easy_localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -8,6 +9,7 @@ import 'provider/go_route_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  await EasyLocalization.ensureInitialized();
 
   // Hive.registerAdapter(OnboardingStateAdapter()); // 생성된 어댑터 등록
 
@@ -17,8 +19,13 @@ void main() async {
   }
 
   runApp(
-    const ProviderScope(
-      child: MainApp(),
+    ProviderScope(
+      child: EasyLocalization(
+        supportedLocales: const [Locale('en', 'US'), Locale('ko', 'KR')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('ko', 'KR'),
+        child: const MainApp(),
+      ),
     ),
   );
 }
@@ -35,6 +42,9 @@ class MainApp extends ConsumerWidget {
       themeMode: ThemeMode.system,
       theme: ThemeData.light(),
       routerConfig: ref.watch(goRouterProvider),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
     );
   }
 }
--- lib/page/onboarding/onboarding_page.dart
+++ lib/page/onboarding/onboarding_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,46 +12,66 @@ class OnboardingPage extends ConsumerWidget {
       home: OnBoardingSlider(
         pageBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
         headerBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        finishButtonText: 'Register',
+        finishButtonText: 'onboarding.start'.tr(),
         finishButtonStyle: FinishButtonStyle(
           backgroundColor: Theme.of(context).primaryColor,
         ),
-        skipTextButton: const Text('Skip'),
-        trailing: const Text('Login'),
+        onFinish: () {
+          // TODO : Go to Auth Page
+        },
+        skipTextButton: const Text('onboarding.skip').tr(),
+        trailing: const Icon(CupertinoIcons.info_circle),
+        trailingFunction: () {
+          // TODO : Help Page
+        },
         background: [
           // Image.asset('assets/slide_1.png'),
           // Image.asset('assets/slide_2.png'),
-          Icon(
-            CupertinoIcons.person,
+          const Icon(
+            CupertinoIcons.person_2,
             size: 100,
             color: Colors.black,
           ),
           Container(
             color: Colors.red,
           ),
+          Container(
+            color: Colors.red,
+          ),
         ],
-        totalPage: 2,
+        totalPage: 3,
         speed: 1.8,
         pageBodies: [
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: const Column(
+            child: Column(
               children: <Widget>[
-                SizedBox(
+                const SizedBox(
                   height: 480,
                 ),
-                Text('Description Text 1'),
+                const Text('onboarding.description.1').tr(),
               ],
             ),
           ),
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: const Column(
+            child: Column(
               children: <Widget>[
-                SizedBox(
+                const SizedBox(
                   height: 480,
                 ),
-                Text('Description Text 2'),
+                const Text('onboarding.description.2').tr(),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 480,
+                ),
+                const Text('onboarding.description.3').tr(),
               ],
             ),
           ),
